16af0f1fa8135afcd7d57c3d8d2d4b45dc2f7f6c
onyekachi chigozie <onyekachichigozie581@gmail.com>
2026-08-08 21:33:07 +0100

    Add ExperimentalMaterial3Api opt-in for TopAppBar

--- app/src/main/java/com/cipherkeys/app/settings/SettingsScreen.kt
+++ app/src/main/java/com/cipherkeys/app/settings/SettingsScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
@@ -30,6 +31,7 @@ import androidx.compose.ui.unit.dp
 import com.cipherkeys.app.data.KeyboardMode
 import com.cipherkeys.app.data.KeyboardTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen(viewModel: SettingsViewModel) {
     val settings by viewModel.uiState.collectAsState()
